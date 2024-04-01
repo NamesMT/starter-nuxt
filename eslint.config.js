2ac1145ec3bdd6f94c5e079d3334596440a5af0a
NamesMT <dangquoctrung123@gmail.com>
2024-04-01 01:30:29 +0000

    chore(eslint.config.js): ignore `primevue`'s assets, use async wrapper to fix `config-inspector`

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,32 @@
 import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default antfu(
-  {
-    unocss: true,
-    ignores: [
-      // eslint ignore globs here
-    ],
-  },
-  // Include @nuxt/eslint configs
-  await withNuxt(),
-  // Allow trailing space for markdown formatting
-  {
-    files: ['*.md'],
-    rules: {
-      'style/no-trailing-spaces': 'off',
+async function asyncWrap() {
+  return antfu(
+    {
+      unocss: true,
+      ignores: [
+        'assets/primevue/**',
+      ],
     },
-  },
-  {
-    rules: {
+    // Include @nuxt/eslint configs
+    await withNuxt(),
+    // Allow trailing space for markdown formatting
+    {
+      files: ['*.md'],
+      rules: {
+        'style/no-trailing-spaces': 'off',
+      },
+    },
+    {
+      rules: {
       // Allow trailing space in comments, for possible JSDoc formattings
-      'style/no-trailing-spaces': ['error', { ignoreComments: true }],
-      // Relaxes inline statements a bit
-      'style/max-statements-per-line': ['error', { max: 2 }],
+        'style/no-trailing-spaces': ['error', { ignoreComments: true }],
+        // Relaxes inline statements a bit
+        'style/max-statements-per-line': ['error', { max: 2 }],
+      },
     },
-  },
-)
+  )
+}
+
+export default asyncWrap()
