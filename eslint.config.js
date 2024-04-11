f0d65141978fae6466146ba104d1b5d869364d94
NamesMT <dangquoctrung123@gmail.com>
2024-04-11 12:03:40 +0000

    refactor(eslint.config): antfu/vitesse-nuxt3#111 closed

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,26 @@
 import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-async function asyncWrap() {
-  return antfu(
-    {
-      unocss: true,
-      ignores: [
-        'assets/primevue/**',
-      ],
+export default withNuxt(antfu(
+  {
+    unocss: true,
+    ignores: [
+      'assets/primevue/**',
+    ],
+  },
+  // Allow trailing space for markdown formatting
+  {
+    files: ['*.md'],
+    rules: {
+      'style/no-trailing-spaces': 'off',
     },
-    // Include @nuxt/eslint configs
-    await withNuxt(),
-    // Allow trailing space for markdown formatting
-    {
-      files: ['*.md'],
-      rules: {
-        'style/no-trailing-spaces': 'off',
-      },
+  },
+  {
+    rules: {
+    // Allow trailing space in comments, for possible JSDoc formattings
+      'style/no-trailing-spaces': ['error', { ignoreComments: true }],
+      // Relaxes inline statements a bit
+      'style/max-statements-per-line': ['error', { max: 2 }],
     },
-    {
-      rules: {
-      // Allow trailing space in comments, for possible JSDoc formattings
-        'style/no-trailing-spaces': ['error', { ignoreComments: true }],
-        // Relaxes inline statements a bit
-        'style/max-statements-per-line': ['error', { max: 2 }],
-      },
-    },
-  )
-}
-
-export default asyncWrap()
+  },
+))
